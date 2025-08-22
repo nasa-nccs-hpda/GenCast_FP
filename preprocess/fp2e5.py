341d5d39b57f78d6e283ef72ad27ac8005b46bbe
JianLi <44811760+jli-99@users.noreply.github.com>
2025-08-22 13:53:22 -0400

    using start and end date

--- preprocess/fp2e5.py
+++ preprocess/fp2e5.py
@@ -2,30 +2,29 @@
 import os
 import numpy as np
 import xarray as xr
-from datetime import datetime
+import pandas as pd
 import xesmf  as xe
 from fp_to_era5 import *
 import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Convert GEOS-FP data to ERA5 format.')
-    parser.add_argument('--year', type=int, required=True, help='Year of the data to process')
-    parser.add_argument('--month', type=int, required=True, help='Month of the data to process')
-    parser.add_argument('--day', type=int, required=True, help='Day of the data to process')
+    parser.add_argument('--start_date', type=str, required=True, help='Start date to process (YYYY-MM-DD)')
+    parser.add_argument('--end_date', type=str, required=True, help='End date to process (YYYY-MM-DD)')
     
     parser.add_argument('--outdir', type=str, default='./output/', help='Output directory for the converted files')
     parser.add_argument('--expid', type=str, default='fp2e5', help='Experiment ID for the output files')
 
     args = parser.parse_args()
-    yyyy = args.year
-    mm  = args.month
-    dd = args.day
+
+    dates = pd.date_range(start=args.start_date, end=args.end_date, freq='12')
+
+
     outdir = args.outdir
     expid = args.expid
 
     regridder = None
-    for hour in [0, 12]:
-        dt = datetime(yyyy, mm, dd, hour)
+    for dt in dates:
         Files = discover_files(dt, outdir=outdir, expid=expid)
         print(Files)
 
