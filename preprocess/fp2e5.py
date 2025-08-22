ee39cce4a1c98983eb6f4c2a4ffdbdae9044a7e5
JianLi <44811760+jli-99@users.noreply.github.com>
2025-08-22 16:31:15 -0400

    reformat

--- preprocess/fp2e5.py
+++ preprocess/fp2e5.py
@@ -7,6 +7,102 @@ import xesmf  as xe
 from fp_to_era5 import *
 import argparse
 
+def expand_dims(ds, steps):
+    # Expand the time dimension of the dataset
+    orig_time = ds.time.values
+    extra_steps = steps-2
+
+    ds_last = ds.isel(time=1)
+    new_times = pd.date_range(start=orig_time[-1], periods=extra_steps+1, freq="12h")[1:]
+
+    repeated = ds_last.expand_dims(time=range(extra_steps)).copy(deep=True)
+    repeated['time'] = new_times
+    ds_extended = xr.concat([ds, repeated], dim='time')
+    return ds_extended
+
+def to_gencast_input(ds):
+    date_str = None
+    nsteps = 22 
+    GRAV = 9.80665
+
+
+    levs = np.array(
+            [50,  100,  150,  200,  250,  \
+            300,  400,  500,  600,  700, \
+            850,  925,  1000])
+
+    static = ["land_sea_mask",
+            "geopotential_at_surface",]
+
+    var_2d = ["2m_temperature",
+            "sea_surface_temperature",
+            "mean_sea_level_pressure",
+            "10m_u_component_of_wind",
+            "10m_v_component_of_wind",
+            "total_precipitation",]
+
+    var_3d = ["temperature",
+            "specific_humidity",
+            "u_component_of_wind",
+            "v_component_of_wind",
+            "vertical_velocity",
+            "geopotential",]
+    var_mapping = {
+                    "t2m": "2m_temperature",
+                    "t": "temperature",
+                    "u10": "10m_u_component_of_wind",
+                    "v10": "10m_v_component_of_wind",
+                    "u": "u_component_of_wind",
+                    "v": "v_component_of_wind",
+                    "q": "specific_humidity",
+                    "w": "vertical_velocity",
+                    "z": "geopotential",
+                    "skt": "sea_surface_temperature",
+                    "msl": "mean_sea_level_pressure",
+                    "tp": "total_precipitation_12hr",
+                    "zs": "geopotential_at_surface",
+                    "latitude": "lat",
+                    "longitude": "lon",
+                    "pressure_level": "level",
+                    }
+    
+    # coarsen the data & reverse the latitude
+    ds = ds.isel(latitude=slice(None, None, -4), longitude=slice(None, None, 4)).compute()
+    res=1.0
+
+    ds = ds.drop_vars(["hgt", "p", "sp"])
+    # change variable names
+    ds = ds.rename(var_mapping)
+
+    # change data types
+    ds = ds.astype({var: 'float32' for var in ds.data_vars})
+
+    # expand the time dimension
+    ds = expand_dims(ds, nsteps)
+
+    ds = ds.assign_coords(datetime=ds["time"])
+
+    # expand the dimensions 
+    for var in list(ds.data_vars) + ['datetime']:
+        ds[var] = ds[var].expand_dims("batch")
+
+    # change time coordinate to timedelta
+    ds['time']=ds['time']-ds['time'].isel(time=0)
+
+    # add land_sea_mask
+    file = f"/discover/nobackup/projects/QEFM/data/FMGenCast/12hr/Y2024/gencast-dataset-source-era5_date-{date_str}_res-1.0_levels-13_steps-20.nc"
+    ds_lsm = xr.open_dataset(file)
+    ds['land_sea_mask'] = ds_lsm['land_sea_mask']
+    # using the sea_surface_temperature from the original dataset
+    ds['sea_surface_temperature'] = ds_lsm['sea_surface_temperature']
+
+    # drop the time dimension for land_sea_mask and geopotential_at_surface
+    #ds['land_sea_mask'] = ds['land_sea_mask'].isel(time=0).drop_vars("time")
+    ds['geopotential_at_surface'] = ds['geopotential_at_surface'].isel(time=0).drop_vars(["time"])
+
+    return ds
+
+
 def main():
     parser = argparse.ArgumentParser(description='Convert GEOS-FP data to ERA5 format.')
     parser.add_argument('--start_date', type=str, required=True, help='Start date to process (YYYY-MM-DD)')
@@ -43,6 +139,10 @@ def main():
                 regridder = xe.Regridder(ai_Nx, ai_Ex, "conservative")
             
             ai_Ex, ai_Ep = fp_to_era5_hgrid(ai_Nx, ai_Np, regridder=regridder)
+
+            e5_Ex = xr.open_dataset(Files['e5_Ex'], engine='netcdf4')
+            print(e5_Ex)
+            exit()
 
             daily_Ex.append(ai_Ex)
             daily_Ep.append(ai_Ep)
