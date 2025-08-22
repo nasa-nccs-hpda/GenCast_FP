877bbd9e50c5a14c2615f5688d6799be12fb5c2a
JianLi <44811760+jli-99@users.noreply.github.com>
2025-08-22 15:57:01 -0400

    merge data after convert

--- preprocess/fp2e5.py
+++ preprocess/fp2e5.py
@@ -26,24 +26,39 @@ def main():
     expid = args.expid
 
     regridder = None
-    for dt in dates:
-        Files = discover_files(dt, outdir=outdir, expid=expid)
-        print(Files)
+    for day, day_dates in dates.groupby(dates.date):
+        daily_Ex = []
+        daily_Ep = []
+        for dt in dates:
+            Files = discover_files(dt, outdir=outdir, expid=expid)
+            print(Files)
 
-        fp_Nx = xr.open_dataset(Files['fp_Nx'], engine='netcdf4')
-        fp_Nv = xr.open_dataset(Files['fp_Nv'], engine='netcdf4')
+            fp_Nx = xr.open_dataset(Files['fp_Nx'], engine='netcdf4')
+            fp_Nv = xr.open_dataset(Files['fp_Nv'], engine='netcdf4')
 
-        ai_Nx, ai_Np = fp_to_era5_xlevs(fp_Nx, fp_Nv)
+            ai_Nx, ai_Np = fp_to_era5_xlevs(fp_Nx, fp_Nv)
 
-        ai_Ex = era5_dataset('GEOS-FP 2D Variables on ERA-5 Grid for AI/ML Modeling')
-        if not regridder:
-            regridder = xe.Regridder(ai_Nx, ai_Ex, "conservative")
+            ai_Ex = era5_dataset('GEOS-FP 2D Variables on ERA-5 Grid for AI/ML Modeling')
+            if not regridder:
+                regridder = xe.Regridder(ai_Nx, ai_Ex, "conservative")
+            
+            ai_Ex, ai_Ep = fp_to_era5_hgrid(ai_Nx, ai_Np, regridder=regridder)
+
+            # add time dimension
+            ai_Ex = ai_Ex.expand_dims(time=[dt])
+            ai_Ep = ai_Ep.expand_dims(time=[dt])
+
+            daily_Ex.append(ai_Ex)
+            daily_Ep.append(ai_Ep)
         
-        ai_Ex, ai_Ep = fp_to_era5_hgrid(ai_Nx, ai_Np, regridder=regridder)
 
-        ai_Ex.to_netcdf(Files['ai_Ex'],engine='netcdf4')
-        ai_Ep.to_netcdf(Files['ai_Ep'],engine='netcdf4')  
+        # concat along time
+        ai_Ex_day = xr.concat(daily_Ex, dim="time")
+        ai_Ep_day = xr.concat(daily_Ep, dim="time")
 
+        # merge into single dataset for the day
+        ai_day = xr.merge([ai_Ex_day, ai_Ep_day])
+        print((ai_day))
 
 
 if __name__ == "__main__":
