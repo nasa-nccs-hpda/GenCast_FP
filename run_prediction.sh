d557d677a48a309b48e597f7cf3e3edd60a3256a
JianLi <44811760+jli-99@users.noreply.github.com>
2025-08-24 20:57:47 -0400

    fix path

--- run_prediction.sh
+++ run_prediction.sh
@@ -5,10 +5,10 @@
 # Load necessary modules
 
 module load singularity
-container="/discover/nobackup/projects/QEFM/containers/qefm-core-gencast.sif"
+container="/discover/nobackup/projects/QEFM/containers/qefm-core-gencast-20250507-sandbox"
 
 # Define working directory and input files
-WORKDIR="/discover/nobackup/jli30/GenCast_FP/predictionFMGenCast"
+WORKDIR="/discover/nobackup/jli30/GenCast_FP/prediction/FMGenCast"
 cd "$WORKDIR" || exit 1
 
 singularity exec "$container" python3 -m fm_gencast.py
