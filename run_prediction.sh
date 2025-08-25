12629408c2bfa43ac70fb3c046315937ad13dec3
JianLi <44811760+jli-99@users.noreply.github.com>
2025-08-24 20:53:19 -0400

    create bash script to run Gencast

---
+++ run_prediction.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script runs the prediction using the GenCast model and GEOS-FP input data.
+
+# Load necessary modules
+
+module load singularity
+container="/discover/nobackup/projects/QEFM/containers/qefm-core-gencast.sif"
+
+# Define working directory and input files
+WORKDIR="/discover/nobackup/jli30/GenCast_FP/predictionFMGenCast"
+cd "$WORKDIR" || exit 1
+
+singularity exec "$container" python3 -m fm_gencast.py
