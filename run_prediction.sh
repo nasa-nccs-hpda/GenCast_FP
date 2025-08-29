f06bc2818656ed6da96c6bebde8162cf9da01d06
JianLi <44811760+jli-99@users.noreply.github.com>
2025-08-29 09:32:04 -0400

    typo

--- run_prediction.sh
+++ run_prediction.sh
@@ -6,7 +6,7 @@
 
 module load singularity
 #container="/discover/nobackup/projects/QEFM/containers/qefm-core-gencast-20250511-sandbox"
-container="/discover/nobackup/projects/QEFM/containers/qefm-core-debian-all-aifs-20250609-sandbox
+container="/discover/nobackup/projects/QEFM/containers/qefm-core-debian-all-aifs-20250609-sandbox"
 
 # Define working directory and input files
 WORKDIR="/discover/nobackup/jli30/GenCast_FP"
