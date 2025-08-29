5761e6601ced93e6c2492062b9b2a01fb54a82d3
JianLi <44811760+jli-99@users.noreply.github.com>
2025-08-29 12:19:23 -0400

    modify the driver script

---
+++ fm_gencast.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+# you can set this to any directory you want
+WORKDIR=$PWD
+
+# Step 1 - Convert GEOS-FP data to ERA5-like format
+bash $WORKDIR/run_preprocess.sh $WORKDIR && echo "Preprocessing done"
+if [ $? -ne 0 ]; then
+    echo "Preprocessing failed"
+    exit 1
+fi
+# Step 2 - Generate forecasts using the preprocessed data
+sbatch $WORKDIR/run_forecast.sbatch $WORKDIR && echo "Forecasting job submitted"
+
