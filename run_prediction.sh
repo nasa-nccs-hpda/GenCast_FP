b6daf333d20c22d3dc6622456500455d89f2df79
JianLi <44811760+jli-99@users.noreply.github.com>
2025-08-29 09:31:26 -0400

    change container

--- run_prediction.sh
+++ run_prediction.sh
@@ -5,7 +5,8 @@
 # Load necessary modules
 
 module load singularity
-container="/discover/nobackup/projects/QEFM/containers/qefm-core-gencast-20250511-sandbox"
+#container="/discover/nobackup/projects/QEFM/containers/qefm-core-gencast-20250511-sandbox"
+container="/discover/nobackup/projects/QEFM/containers/qefm-core-debian-all-aifs-20250609-sandbox
 
 # Define working directory and input files
 WORKDIR="/discover/nobackup/jli30/GenCast_FP"
