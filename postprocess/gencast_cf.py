4015e7f9133b77617dc6184d4798b521cedf0d7b
JianLi <44811760+jli-99@users.noreply.github.com>
2025-09-01 16:17:45 -0400

    np datetime

--- postprocess/gencast_cf.py
+++ postprocess/gencast_cf.py
@@ -227,7 +227,7 @@ def main():
     ds = xr.open_dataset(files[0])
     ds = ds.drop_vars("land_sea_mask", errors='ignore')
     for ctime in ds.time.values[:n]:
-        print("Processing time ", ctime.strftime("%Y-%m-%d"), " for case ", case)
+        print("Processing time ", str(ctime.astype("datetime64[D]")), " for case ", case)
         proc_time_step(ds, ctime, ref_date, output_dir=output_dir, case=case)
 
 
