a7016793f0afec549f074b508b9b89869f91c8cd
JianLi <44811760+jli-99@users.noreply.github.com>
2025-09-27 21:39:37 -0400

    check sst

--- gencast_fp/preprocess/fp2e5.py
+++ gencast_fp/preprocess/fp2e5.py
@@ -222,6 +222,10 @@ def run_preprocess(start_date, end_date, outdir, expid):
             Files = discover_files(dt, outdir=outdir, expid=expid)
             logging.info(Files)
 
+            sst_ds = get_sst(Files["sst"], dt)
+            sst_ds.to_netcdf('sst.nc')
+            exit()
+
             fp_Nx = xr.open_dataset(Files["fp_Nx"], engine="netcdf4")
             fp_Nv = xr.open_dataset(Files["fp_Nv"], engine="netcdf4")
 
